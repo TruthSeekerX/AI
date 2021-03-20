016ac5c06b1cf3dd5b526e7c4831e27ab680b6c0
TruthSeekerX <siyuan.xu@outlook.com>
2021-03-20 20:51:22 +0200

    ai_io.c is removed for simplify the dependencies

--- ai_io.c
+++
@@ -1,9 +0,0 @@
-/********************************************************************************************************************************************
-* This file is part of the Neural Nod project. It is meant for first year C programming introduction course lectured by Johan Dams.
-* It includes varias input output functions:
-	1.   
-* Language: C
-* Author:Siyuan e2002077
-* Version: v0.1
-* 10.03.2021
-*********************************************************************************************************************************************/
